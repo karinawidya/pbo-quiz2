05ca0986087b79b55c046aedc6d57b6289492f0e
Karina Widya <karinawidya9999@gmail.com>
2018-11-26 18:32:15 +0700

    Menambahkan atribut dan konstruktor di class Transaksi

--- src/com/karina/library/Transaksi.java
+++ src/com/karina/library/Transaksi.java
@@ -4,11 +4,23 @@
  * and open the template in the editor.
  */
 package com.karina.library;
-
+import java.util.*;
 /**
  *
  * @author ASUS
  */
 public class Transaksi {
+    
+    private String code;//variable code untuk transaksi code
+    private ArrayList<Item> items = new ArrayList<>();//variable items untuk menyimpan list item
+    private float total;//variable total untuk menyimpan total
+    
+    //konstruktor berparameter
+    public Transaksi(String code, float total) {
+        this.code = code;
+        this.total = total;
+    }
+    
+    
     
 }
